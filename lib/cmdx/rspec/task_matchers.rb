73ddc4e2cba2f824a20737ea1486d829739c5ca8
Juan Gomez <drexed@users.noreply.github.com>
2025-07-10 11:15:02 -0400

    Add a have_parameter matcher

--- lib/cmdx/rspec/task_matchers.rb
+++ lib/cmdx/rspec/task_matchers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative "task_matchers/use_default_value"
+require_relative "task_matchers/be_well_formed_task"
+require_relative "task_matchers/have_task_setting"
 require_relative "task_matchers/have_middleware"
 require_relative "task_matchers/have_callback"
+require_relative "task_matchers/have_parameter"
 require_relative "task_matchers/have_executed_callbacks"
-require_relative "task_matchers/have_task_setting"
-require_relative "task_matchers/be_well_formed_task"
