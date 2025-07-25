bb0cd8093271730d4f6c457f4daa9b14165d1ab2
Juan Gomez <drexed@users.noreply.github.com>
2025-07-24 23:01:31 -0400

    Update parameter_attribute.rb

--- lib/cmdx/parameter_attribute.rb
+++ lib/cmdx/parameter_attribute.rb
@@ -3,11 +3,12 @@
 module CMDx
   class ParameterAttribute
 
-    attr_reader :task, :parameter
+    attr_reader :task, :parameter, :errors
 
     def initialize(task, parameter)
       @task      = task
       @parameter = parameter
+      @errors    = Set.new
     end
 
     def self.call(task, parameter)
@@ -15,7 +16,17 @@ module CMDx
     end
 
     def call
-      derived_value
+      source_value
+      return unless errors.empty?
+
+      derive_value
+      coerce_value
+      return unless errors.empty?
+
+      validate_value
+      return unless errors.empty?
+
+      @derived_value
     end
 
     private
@@ -25,12 +36,10 @@ module CMDx
 
       @source_value =
         case parameter.source
-        when Symbol, String
-          task.send(parameter.source)
-        when Proc
-          parameter.source.call(task)
+        when Symbol, String then task.send(parameter.source)
+        when Proc then parameter.source.call(task)
         else
-          raise ValidationError, I18n.t(
+          errors << I18n.t(
             "cmdx.parameters.undefined",
             default: "delegates to undefined source #{parameter.source}",
             source: parameter.source
@@ -40,75 +49,67 @@ module CMDx
       if !@source_value.nil? || parameter.parent&.optional? || parameter.optional?
         @source_value
       else
-        raise ValidationError, I18n.t(
+        errors << I18n.t(
           "cmdx.parameters.required",
           default: "is a required parameter"
         )
       end
     end
 
-    def default_value
-      case default = parameter.options[:default]
-      when Proc
-        default.call(task)
-      else
-        default
-      end
-    end
-
-    def derived_value
+    def derive_value
       return @derived_value if defined?(@derived_value)
 
       @derived_value =
         case source_value
-        when Proc
-          source_value.call(task)
-        when Context, Hash
-          source_value[parameter.name]
-        else
-          source_value.send(parameter.name)
+        when Context, Hash then source_value[parameter.name]
+        when Proc then source_value.call(task)
+        else source_value.send(parameter.name)
         end
 
       if @derived_value.nil?
-        @derived_value = default_value
+        @derived_value =
+          case default = parameter.options[:default]
+          when Proc then default.call(task)
+          else default
+          end
       else
         @derived_value
       end
     end
 
-    # def coerce_value
-    #   types = Array(parameter.type)
-    #   tsize = types.size - 1
+    def coerce_value
+      #   types = Array(parameter.type)
+      #   tsize = types.size - 1
 
-    #   types.each_with_index do |key, i|
-    #     break parameter.klass.settings[:coercions].call(task, key, value, options)
-    #   rescue CoercionError => e
-    #     next if tsize != i
+      #   types.each_with_index do |key, i|
+      #     break parameter.klass.settings[:coercions].call(task, key, value, options)
+      #   rescue CoercionError => e
+      #     next if tsize != i
 
-    #     raise(e) if tsize.zero?
+      #     raise(e) if tsize.zero?
 
-    #     values = types.map(&:to_s).join(", ")
-    #     raise CoercionError, I18n.t(
-    #       "cmdx.coercions.into_any",
-    #       values:,
-    #       default: "could not coerce into one of: #{values}"
-    #     )
-    #   end
-    # end
+      #     values = types.map(&:to_s).join(", ")
+      #     raise CoercionError, I18n.t(
+      #       "cmdx.coercions.into_any",
+      #       values:,
+      #       default: "could not coerce into one of: #{values}"
+      #     )
+      #   end
+    end
 
-    # def validate_value
-    #   return if skip_validations_due_to_optional_missing_argument?
+    def validate_value
+      #   return if skip_validations_due_to_optional_missing_argument?
 
-    #   types = CMDx.configuration.validators.registry.keys
+      #   types = CMDx.configuration.validators.registry.keys
 
-    #   options.slice(*types).each_key do |key|
-    #     opts = options[key]
-    #     next if skip_validator_due_to_allow_nil?(opts)
-    #     next if skip_validator_due_to_conditional?(opts)
+      #   options.slice(*types).each_key do |key|
+      #     opts = options[key]
+      #     next if skip_validator_due_to_allow_nil?(opts)
+      #     next if skip_validator_due_to_conditional?(opts)
 
-    #     CMDx.configuration.validators.call(task, key, value, opts)
-    #   end
-    # end
+      #     CMDx.configuration.validators.call(task, key, value, opts)
+      #   end
+    end
 
   end
 end
