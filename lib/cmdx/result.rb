7daddc0ed4aeff0f9e2ac5c134bbd5b92460d31d
Juan Gomez <drexed@users.noreply.github.com>
2025-07-28 23:40:58 -0400

    Update result.rb

--- lib/cmdx/result.rb
+++ lib/cmdx/result.rb
@@ -3,6 +3,8 @@
 module CMDx
   class Result
 
+    extend Forwardable
+
     STATES = [
       INITIALIZED = "initialized",  # Initial state before execution
       EXECUTING   = "executing",    # Currently executing task logic
@@ -15,8 +17,203 @@ module CMDx
       FAILED  = "failed"    # Task failed due to error or validation
     ].freeze
 
+    def_delegators :task, :context, :chain
+
+    attr_reader :task, :state, :status, :metadata
+
     def initialize(task)
-      @task = task
+      raise TypeError, "must be a Task or Workflow" unless task.is_a?(Task)
+
+      @task     = task
+      @state    = INITIALIZED
+      @status   = SUCCESS
+      @metadata = {}
+    end
+
+    STATES.each do |s|
+      define_method(:"#{s}?") { state == s }
+
+      define_method(:"on_#{s}") do |&block|
+        raise ArgumentError, "block required" unless block
+
+        block.call(self) if send(:"#{s}?")
+        self
+      end
+    end
+
+    def executed!
+      success? ? complete! : interrupt!
+    end
+
+    def executed?
+      complete? || interrupted?
+    end
+
+    def on_executed(&)
+      raise ArgumentError, "block required" unless block_given?
+
+      yield(self) if executed?
+      self
+    end
+
+    def executing!
+      return if executing?
+
+      raise "can only transition to #{EXECUTING} from #{INITIALIZED}" unless initialized?
+
+      @state = EXECUTING
+    end
+
+    def complete!
+      return if complete?
+
+      raise "can only transition to #{COMPLETE} from #{EXECUTING}" unless executing?
+
+      @state = COMPLETE
+    end
+
+    def interrupt!
+      return if interrupted?
+
+      raise "cannot transition to #{INTERRUPTED} from #{COMPLETE}" if complete?
+
+      @state = INTERRUPTED
+    end
+
+    STATUSES.each do |s|
+      define_method(:"#{s}?") { status == s }
+
+      define_method(:"on_#{s}") do |&block|
+        raise ArgumentError, "block required" unless block
+
+        block.call(self) if send(:"#{s}?")
+        self
+      end
+    end
+
+    def good?
+      !failed?
+    end
+
+    def on_good(&)
+      raise ArgumentError, "block required" unless block_given?
+
+      yield(self) if good?
+      self
+    end
+
+    def bad?
+      !success?
+    end
+
+    def on_bad(&)
+      raise ArgumentError, "block required" unless block_given?
+
+      yield(self) if bad?
+      self
+    end
+
+    def skip!(**metadata)
+      return if skipped?
+
+      raise "can only transition to #{SKIPPED} from #{SUCCESS}" unless success?
+
+      @status   = SKIPPED
+      @metadata = metadata
+
+      halt! unless metadata[:original_exception]
+    end
+
+    def fail!(**metadata)
+      return if failed?
+
+      raise "can only transition to #{FAILED} from #{SUCCESS}" unless success?
+
+      @status   = FAILED
+      @metadata = metadata
+
+      halt! unless metadata[:original_exception]
+    end
+
+    def halt!
+      return if success?
+
+      raise Fault.build(self)
+    end
+
+    def throw!(result, local_metadata = {})
+      raise TypeError, "must be a Result" unless result.is_a?(Result)
+
+      md = result.metadata.merge(local_metadata)
+
+      skip!(**md) if result.skipped?
+      fail!(**md) if result.failed?
+    end
+
+    def caused_failure
+      return unless failed?
+
+      chain.results.reverse.find(&:failed?)
+    end
+
+    def caused_failure?
+      return false unless failed?
+
+      caused_failure == self
+    end
+
+    def threw_failure
+      return unless failed?
+
+      results = chain.results.select(&:failed?)
+      results.find { |r| r.index > index } || results.last
+    end
+
+    def threw_failure?
+      return false unless failed?
+
+      threw_failure == self
+    end
+
+    def thrown_failure?
+      failed? && !caused_failure?
+    end
+
+    def index
+      chain.index(self)
+    end
+
+    def outcome
+      initialized? || thrown_failure? ? state : status
+    end
+
+    def runtime(&)
+      return @runtime unless block_given?
+
+      @runtime = Utils::MonotonicRuntime.call(&)
+    end
+
+    def to_h
+      # TODO
+    end
+
+    def to_s
+      # TODO
+    end
+
+    def deconstruct(*)
+      [state, status]
+    end
+
+    def deconstruct_keys(*)
+      {
+        state: state,
+        status: status,
+        metadata: metadata,
+        executed: executed?,
+        good: good?,
+        bad: bad?
+      }
     end
 
   end
