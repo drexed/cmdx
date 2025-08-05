74d7b2867a7752ea3344be2ae2d7727a65617e17
Juan Gomez <drexed@users.noreply.github.com>
2025-08-05 18:20:41 -0400

    Update i18n.rb

--- spec/support/config/i18n.rb
+++ spec/support/config/i18n.rb
@@ -2,7 +2,7 @@
 
 require "i18n"
 
-locales = Dir[CMDx.gem_path.join("locales/*.yml")]
+locales = Dir[CMDx.gem_path.join("lib/locales/*.yml")]
 
 I18n.load_path += locales
 I18n.available_locales = locales.map { |path| File.basename(path, ".yml").to_sym }
