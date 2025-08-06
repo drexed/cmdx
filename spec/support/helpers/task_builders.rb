55718ffe6da9c661d1bdaa6feed64e2bbd6b21cb
Juan Gomez <drexed@users.noreply.github.com>
2025-08-05 21:34:44 -0400

    Clean up task builders

--- spec/support/helpers/task_builders.rb
+++ spec/support/helpers/task_builders.rb
@@ -6,53 +6,37 @@ module CMDx
 
       def create_task_class(base: nil, name: "AnonymousTask", &block)
         task_class = Class.new(base || CMDx::Task)
-        task_class.define_singleton_method(:name) do
-          hash = rand(10_000).to_s.rjust(4, "0")
-          "#{name}#{hash}"
-        end
+        task_class.define_singleton_method(:name) { name.to_s << rand(9999).to_s.rjust(4, "0") }
         task_class.class_eval(&block) if block_given?
         task_class
       end
 
-      def create_simple_task(base: nil, name: "SimpleTask", &block)
-        create_task_class(name:, base:) do
-          define_method :call do
-            context.executed = true
-          end
-
-          class_eval(&block) if block_given?
-        end
+      def create_successful_task(base: nil, name: "SuccessfulTask", &block)
+        task_class = create_task_class(base:, name:)
+        task_class.define_method(:call) { context.executed = true }
+        task_class.class_eval(&block) if block_given?
+        task_class
       end
-      alias create_successful_task create_simple_task
 
       def create_failing_task(base: nil, name: "FailingTask", reason: "Task failed", **metadata, &block)
-        create_task_class(name:, base:) do
-          define_method :call do
-            fail!(reason: reason, **metadata)
-          end
-
-          class_eval(&block) if block_given?
-        end
+        task_class = create_task_class(base:, name:)
+        task_class.define_method(:call) { fail!(reason, **metadata) }
+        task_class.class_eval(&block) if block_given?
+        task_class
       end
 
       def create_skipping_task(base: nil, name: "SkippingTask", reason: "Task skipped", **metadata, &block)
-        create_task_class(name:, base:) do
-          define_method :call do
-            skip!(reason: reason, **metadata)
-          end
-
-          class_eval(&block) if block_given?
-        end
+        task_class = create_task_class(base:, name:)
+        task_class.define_method(:call) { skip!(reason, **metadata) }
+        task_class.class_eval(&block) if block_given?
+        task_class
       end
 
       def create_erroring_task(base: nil, name: "ErroringTask", reason: "Task errored", **_metadata, &block)
-        create_task_class(name:, base:) do
-          define_method :call do
-            raise StandardError, reason
-          end
-
-          class_eval(&block) if block_given?
-        end
+        task_class = create_task_class(base:, name:)
+        task_class.define_method(:call) { raise StandardError, reason }
+        task_class.class_eval(&block) if block_given?
+        task_class
       end
 
     end
