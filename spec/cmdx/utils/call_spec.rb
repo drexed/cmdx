ac63df9aff04b204a67751b01b1dc893c3c82df2
Juan Gomez <drexed@users.noreply.github.com>
2025-08-06 12:01:34 -0400

    Add specs

---
+++ spec/cmdx/utils/call_spec.rb
@@ -0,0 +1,240 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CMDx::Utils::Call do
+  subject(:call_module) { described_class }
+
+  let(:target_object) do
+    instance_double("Target", test_method: "method_result", instance_variable_set: nil)
+  end
+
+  describe ".invoke" do
+    context "when callable is a Symbol" do
+      let(:callable) { :test_method }
+
+      it "calls the method on the target object" do
+        expect(target_object).to receive(:test_method).with(no_args)
+
+        call_module.invoke(target_object, callable)
+      end
+
+      it "passes arguments to the method" do
+        args = %w[arg1 arg2]
+
+        expect(target_object).to receive(:test_method).with(*args)
+
+        call_module.invoke(target_object, callable, *args)
+      end
+
+      it "passes keyword arguments to the method" do
+        kwargs = { key1: "value1", key2: "value2" }
+
+        expect(target_object).to receive(:test_method).with(**kwargs)
+
+        call_module.invoke(target_object, callable, **kwargs)
+      end
+
+      it "passes both arguments and keyword arguments to the method" do
+        args = ["arg1"]
+        kwargs = { key1: "value1" }
+
+        expect(target_object).to receive(:test_method).with(*args, **kwargs)
+
+        call_module.invoke(target_object, callable, *args, **kwargs)
+      end
+
+      it "passes blocks to the method" do
+        block = proc { "block_result" }
+
+        expect(target_object).to receive(:test_method) do |&passed_block|
+          expect(passed_block).to eq(block)
+        end
+
+        call_module.invoke(target_object, callable, &block)
+      end
+
+      it "returns the result of the method call" do
+        allow(target_object).to receive(:test_method).and_return("expected_result")
+
+        result = call_module.invoke(target_object, callable)
+
+        expect(result).to eq("expected_result")
+      end
+    end
+
+    context "when callable is a Proc" do
+      let(:callable) { proc { |*args, **kwargs| "proc_result_#{args.join('_')}_#{kwargs.values.join('_')}" } }
+
+      it "executes the proc in the context of the target object" do
+        expect(target_object).to receive(:instance_exec).with(no_args) do |&block|
+          expect(block).to eq(callable)
+          "instance_exec_result"
+        end
+
+        call_module.invoke(target_object, callable)
+      end
+
+      it "passes arguments to instance_exec" do
+        args = %w[arg1 arg2]
+
+        expect(target_object).to receive(:instance_exec).with(*args, &callable)
+
+        call_module.invoke(target_object, callable, *args)
+      end
+
+      it "passes keyword arguments to instance_exec" do
+        kwargs = { key1: "value1", key2: "value2" }
+
+        expect(target_object).to receive(:instance_exec).with(**kwargs, &callable)
+
+        call_module.invoke(target_object, callable, **kwargs)
+      end
+
+      it "passes both arguments and keyword arguments to instance_exec" do
+        args = ["arg1"]
+        kwargs = { key1: "value1" }
+
+        expect(target_object).to receive(:instance_exec).with(*args, **kwargs, &callable)
+
+        call_module.invoke(target_object, callable, *args, **kwargs)
+      end
+
+      it "passes blocks to instance_exec" do
+        block = proc { "block_result" }
+
+        expect(target_object).to receive(:instance_exec) do |&passed_block|
+          expect(passed_block).to eq(callable)
+        end
+
+        call_module.invoke(target_object, callable, &block)
+      end
+
+      it "returns the result of the proc execution" do
+        allow(target_object).to receive(:instance_exec).and_return("proc_execution_result")
+
+        result = call_module.invoke(target_object, callable)
+
+        expect(result).to eq("proc_execution_result")
+      end
+    end
+
+    context "when callable responds to call" do
+      let(:callable) { double("callable", call: "callable_result") }
+
+      before do
+        allow(callable).to receive(:respond_to?).with(:call).and_return(true)
+      end
+
+      it "calls the call method on the callable object" do
+        expect(callable).to receive(:call).with(no_args)
+
+        call_module.invoke(target_object, callable)
+      end
+
+      it "passes arguments to the callable object" do
+        args = %w[arg1 arg2]
+
+        expect(callable).to receive(:call).with(*args)
+
+        call_module.invoke(target_object, callable, *args)
+      end
+
+      it "passes keyword arguments to the callable object" do
+        kwargs = { key1: "value1", key2: "value2" }
+
+        expect(callable).to receive(:call).with(**kwargs)
+
+        call_module.invoke(target_object, callable, **kwargs)
+      end
+
+      it "passes both arguments and keyword arguments to the callable object" do
+        args = ["arg1"]
+        kwargs = { key1: "value1" }
+
+        expect(callable).to receive(:call).with(*args, **kwargs)
+
+        call_module.invoke(target_object, callable, *args, **kwargs)
+      end
+
+      it "passes blocks to the callable object" do
+        block = proc { "block_result" }
+
+        expect(callable).to receive(:call) do |&passed_block|
+          expect(passed_block).to eq(block)
+        end
+
+        call_module.invoke(target_object, callable, &block)
+      end
+
+      it "returns the result of the callable object call" do
+        allow(callable).to receive(:call).and_return("callable_object_result")
+
+        result = call_module.invoke(target_object, callable)
+
+        expect(result).to eq("callable_object_result")
+      end
+    end
+
+    context "when callable is a lambda" do
+      let(:callable) { ->(arg) { "lambda_result_#{arg}" } }
+
+      it "calls the lambda directly" do
+        result = call_module.invoke(target_object, callable, "test")
+
+        expect(result).to eq("lambda_result_test")
+      end
+    end
+
+    context "when callable is a method object" do
+      let(:method_owner) { Object.new.tap { |o| o.define_singleton_method(:test) { |arg| "method_result_#{arg}" } } }
+      let(:callable) { method_owner.method(:test) }
+
+      it "calls the method object" do
+        result = call_module.invoke(target_object, callable, "test")
+
+        expect(result).to eq("method_result_test")
+      end
+    end
+
+    context "when callable is invalid" do
+      let(:invalid_callable) { "not_callable" }
+
+      it "raises an error with a descriptive message" do
+        expect { call_module.invoke(target_object, invalid_callable) }
+          .to raise_error(RuntimeError, "cannot invoke not_callable")
+      end
+    end
+
+    context "when callable is nil" do
+      let(:callable) { nil }
+
+      it "raises an error" do
+        expect { call_module.invoke(target_object, callable) }
+          .to raise_error(RuntimeError, "cannot invoke ")
+      end
+    end
+
+    context "when callable is an integer" do
+      let(:callable) { 42 }
+
+      it "raises an error" do
+        expect { call_module.invoke(target_object, callable) }
+          .to raise_error(RuntimeError, "cannot invoke 42")
+      end
+    end
+
+    context "when callable responds to call but returns false" do
+      let(:callable) { instance_double("NotCallable") }
+
+      before do
+        allow(callable).to receive(:respond_to?).with(:call).and_return(false)
+      end
+
+      it "raises an error" do
+        expect { call_module.invoke(target_object, callable) }
+          .to raise_error(RuntimeError, /cannot invoke/)
+      end
+    end
+  end
+end
