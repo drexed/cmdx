f23a3fd132c219d59c6bb85cb07c1f89f8c186a5
Juan Gomez <drexed@users.noreply.github.com>
2025-08-07 09:59:23 -0400

    more specs

--- spec/cmdx/configuration_spec.rb
+++ spec/cmdx/configuration_spec.rb
@@ -3,262 +3,232 @@
 require "spec_helper"
 
 RSpec.describe CMDx::Configuration do
-  subject { described_class.new }
+  subject(:configuration) { described_class.new }
 
   describe "#initialize" do
-    it "sets up default middlewares registry" do
-      expect(subject.middlewares).to be_a(CMDx::MiddlewareRegistry)
-      expect(subject.middlewares.registry).to eq([])
+    it "initializes middlewares registry" do
+      expect(configuration.middlewares).to be_a(CMDx::MiddlewareRegistry)
+      expect(configuration.middlewares.registry).to be_empty
     end
 
-    it "sets up default callbacks registry" do
-      expect(subject.callbacks).to be_a(CMDx::CallbackRegistry)
-      expect(subject.callbacks.registry).to eq({})
+    it "initializes callbacks registry" do
+      expect(configuration.callbacks).to be_a(CMDx::CallbackRegistry)
+      expect(configuration.callbacks.registry).to be_empty
     end
 
-    it "sets up default coercions registry" do
-      expect(subject.coercions).to be_a(CMDx::CoercionRegistry)
-      expect(subject.coercions.registry).to include(
+    it "initializes coercions registry with default coercions" do
+      expect(configuration.coercions).to be_a(CMDx::CoercionRegistry)
+      expect(configuration.coercions.registry).to include(
         array: CMDx::Coercions::Array,
         boolean: CMDx::Coercions::Boolean,
         string: CMDx::Coercions::String,
-        integer: CMDx::Coercions::Integer
+        integer: CMDx::Coercions::Integer,
+        float: CMDx::Coercions::Float,
+        hash: CMDx::Coercions::Hash,
+        big_decimal: CMDx::Coercions::BigDecimal,
+        complex: CMDx::Coercions::Complex,
+        date: CMDx::Coercions::Date,
+        datetime: CMDx::Coercions::DateTime,
+        rational: CMDx::Coercions::Rational,
+        time: CMDx::Coercions::Time
       )
     end
 
-    it "sets up default validators registry" do
-      expect(subject.validators).to be_a(CMDx::ValidatorRegistry)
-      expect(subject.validators.registry).to include(
+    it "initializes validators registry with default validators" do
+      expect(configuration.validators).to be_a(CMDx::ValidatorRegistry)
+      expect(configuration.validators.registry).to include(
         presence: CMDx::Validators::Presence,
         format: CMDx::Validators::Format,
-        inclusion: CMDx::Validators::Inclusion
+        inclusion: CMDx::Validators::Inclusion,
+        exclusion: CMDx::Validators::Exclusion,
+        length: CMDx::Validators::Length,
+        numeric: CMDx::Validators::Numeric
       )
     end
 
-    it "sets task_breakpoints to default values" do
-      expect(subject.task_breakpoints).to eq(%w[failed])
+    it "sets breakpoints to default values" do
+      expect(configuration.task_breakpoints).to eq(%w[failed])
+      expect(configuration.workflow_breakpoints).to eq(%w[failed])
     end
 
-    it "sets workflow_breakpoints to default values" do
-      expect(subject.workflow_breakpoints).to eq(%w[failed])
-    end
+    it "initializes logger with default configuration" do
+      logger = configuration.logger
 
-    it "sets up default logger with correct configuration" do
-      expect(subject.logger).to be_a(Logger)
-      expect(subject.logger.progname).to eq("cmdx")
-      expect(subject.logger.formatter).to be_a(CMDx::LogFormatters::Line)
-      expect(subject.logger.level).to eq(Logger::INFO)
-    end
-  end
-
-  describe "#middlewares=" do
-    let(:custom_registry) { CMDx::MiddlewareRegistry.new }
-
-    it "sets middlewares to the provided value" do
-      subject.middlewares = custom_registry
-      expect(subject.middlewares).to eq(custom_registry)
-    end
-  end
-
-  describe "#callbacks=" do
-    let(:custom_registry) { CMDx::CallbackRegistry.new }
-
-    it "sets callbacks to the provided value" do
-      subject.callbacks = custom_registry
-      expect(subject.callbacks).to eq(custom_registry)
-    end
-  end
-
-  describe "#coercions=" do
-    let(:custom_registry) { CMDx::CoercionRegistry.new }
-
-    it "sets coercions to the provided value" do
-      subject.coercions = custom_registry
-      expect(subject.coercions).to eq(custom_registry)
-    end
-  end
-
-  describe "#validators=" do
-    let(:custom_registry) { CMDx::ValidatorRegistry.new }
-
-    it "sets validators to the provided value" do
-      subject.validators = custom_registry
-      expect(subject.validators).to eq(custom_registry)
-    end
-  end
-
-  describe "#task_breakpoints=" do
-    let(:custom_breakpoints) { %w[failed error] }
-
-    it "sets task_breakpoints to the provided value" do
-      subject.task_breakpoints = custom_breakpoints
-      expect(subject.task_breakpoints).to eq(custom_breakpoints)
-    end
-  end
-
-  describe "#workflow_breakpoints=" do
-    let(:custom_breakpoints) { %w[failed timeout] }
-
-    it "sets workflow_breakpoints to the provided value" do
-      subject.workflow_breakpoints = custom_breakpoints
-      expect(subject.workflow_breakpoints).to eq(custom_breakpoints)
-    end
-  end
-
-  describe "#logger=" do
-    let(:custom_logger) { Logger.new($stderr) }
-
-    it "sets logger to the provided value" do
-      subject.logger = custom_logger
-      expect(subject.logger).to eq(custom_logger)
-    end
-  end
-
-  describe "#to_h" do
-    it "returns a hash with all configuration attributes" do
-      result = subject.to_h
-
-      expect(result).to include(
-        middlewares: subject.middlewares,
-        callbacks: subject.callbacks,
-        coercions: subject.coercions,
-        validators: subject.validators,
-        task_breakpoints: subject.task_breakpoints,
-        workflow_breakpoints: subject.workflow_breakpoints,
-        logger: subject.logger
-      )
-    end
-
-    it "returns exactly 7 keys" do
-      expect(subject.to_h.keys.size).to eq(7)
-    end
-
-    context "when attributes are modified" do
-      let(:custom_breakpoints) { %w[custom] }
-      let(:custom_logger) { Logger.new($stderr) }
-
-      before do
-        subject.task_breakpoints = custom_breakpoints
-        subject.logger = custom_logger
-      end
-
-      it "returns the modified values" do
-        result = subject.to_h
-
-        expect(result[:task_breakpoints]).to eq(custom_breakpoints)
-        expect(result[:logger]).to eq(custom_logger)
-      end
-    end
-  end
-
-  describe "DEFAULT_BREAKPOINTS" do
-    it "is frozen" do
-      expect(described_class::DEFAULT_BREAKPOINTS).to be_frozen
-    end
-
-    it "contains expected values" do
-      expect(described_class::DEFAULT_BREAKPOINTS).to eq(%w[failed])
-    end
-  end
-end
-
-RSpec.describe CMDx do
-  describe ".configuration" do
-    it "returns a Configuration instance" do
-      expect(CMDx.configuration).to be_a(CMDx::Configuration)
-    end
-
-    it "returns the same instance on subsequent calls" do
-      first_call = CMDx.configuration
-      second_call = CMDx.configuration
-
-      expect(first_call).to be(second_call)
-    end
-
-    context "when configuration is already set" do
-      let(:custom_config) { CMDx::Configuration.new }
-
-      before do
-        CMDx.instance_variable_set(:@configuration, custom_config)
-      end
-
-      it "returns the existing configuration" do
-        expect(CMDx.configuration).to be(custom_config)
-      end
-    end
-  end
-
-  describe ".configure" do
-    it "yields the configuration object" do
-      expect { |b| CMDx.configure(&b) }.to yield_with_args(CMDx::Configuration)
-    end
-
-    it "returns the configuration object" do
-      result = CMDx.configure { |config| }
-      expect(result).to be(CMDx.configuration)
-    end
-
-    it "allows modification of configuration within the block" do
-      custom_breakpoints = %w[custom_failed]
-
-      CMDx.configure do |config|
-        config.task_breakpoints = custom_breakpoints
-      end
-
-      expect(CMDx.configuration.task_breakpoints).to eq(custom_breakpoints)
-    end
-
-    context "without a block" do
-      it "raises ArgumentError" do
-        expect { CMDx.configure }.to raise_error(ArgumentError, "block required")
-      end
-    end
-  end
-
-  describe ".reset_configuration!" do
-    before do
-      # Modify the configuration first
-      CMDx.configure do |config|
-        config.task_breakpoints = %w[custom]
-        config.logger = Logger.new($stderr)
-      end
-    end
-
-    it "creates a new Configuration instance" do
-      original_config = CMDx.configuration
-      CMDx.reset_configuration!
-
-      expect(CMDx.configuration).not_to be(original_config)
-      expect(CMDx.configuration).to be_a(CMDx::Configuration)
-    end
-
-    it "resets task_breakpoints to default values" do
-      CMDx.reset_configuration!
-      expect(CMDx.configuration.task_breakpoints).to eq(%w[failed])
-    end
-
-    it "resets workflow_breakpoints to default values" do
-      CMDx.reset_configuration!
-      expect(CMDx.configuration.workflow_breakpoints).to eq(%w[failed])
-    end
-
-    it "resets logger to default configuration" do
-      CMDx.reset_configuration!
-      logger = CMDx.configuration.logger
-
+      expect(logger).to be_a(Logger)
       expect(logger.progname).to eq("cmdx")
       expect(logger.formatter).to be_a(CMDx::LogFormatters::Line)
       expect(logger.level).to eq(Logger::INFO)
     end
+  end
 
-    it "resets registries to their default state" do
-      CMDx.reset_configuration!
-      config = CMDx.configuration
+  describe "attribute accessors" do
+    describe "#middlewares" do
+      let(:custom_registry) { CMDx::MiddlewareRegistry.new }
 
-      expect(config.middlewares.registry).to eq([])
-      expect(config.callbacks.registry).to eq({})
-      expect(config.coercions.registry).to include(:string, :integer, :boolean)
-      expect(config.validators.registry).to include(:presence, :format, :inclusion)
+      it "allows setting and getting middlewares" do
+        configuration.middlewares = custom_registry
+        expect(configuration.middlewares).to eq(custom_registry)
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          expect { configuration.middlewares = nil }.not_to raise_error
+          expect(configuration.middlewares).to be_nil
+        end
+      end
+    end
+
+    describe "#callbacks" do
+      let(:custom_registry) { CMDx::CallbackRegistry.new }
+
+      it "allows setting and getting callbacks" do
+        configuration.callbacks = custom_registry
+        expect(configuration.callbacks).to eq(custom_registry)
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          expect { configuration.callbacks = nil }.not_to raise_error
+          expect(configuration.callbacks).to be_nil
+        end
+      end
+    end
+
+    describe "#coercions" do
+      let(:custom_registry) { CMDx::CoercionRegistry.new }
+
+      it "allows setting and getting coercions" do
+        configuration.coercions = custom_registry
+        expect(configuration.coercions).to eq(custom_registry)
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          expect { configuration.coercions = nil }.not_to raise_error
+          expect(configuration.coercions).to be_nil
+        end
+      end
+    end
+
+    describe "#validators" do
+      let(:custom_registry) { CMDx::ValidatorRegistry.new }
+
+      it "allows setting and getting validators" do
+        configuration.validators = custom_registry
+        expect(configuration.validators).to eq(custom_registry)
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          expect { configuration.validators = nil }.not_to raise_error
+          expect(configuration.validators).to be_nil
+        end
+      end
+    end
+
+    describe "#task_breakpoints" do
+      let(:custom_breakpoints) { %w[failed error timeout] }
+
+      it "allows setting and getting task_breakpoints" do
+        configuration.task_breakpoints = custom_breakpoints
+        expect(configuration.task_breakpoints).to eq(custom_breakpoints)
+      end
+
+      context "with empty array" do
+        it "accepts empty array assignment" do
+          configuration.task_breakpoints = []
+          expect(configuration.task_breakpoints).to eq([])
+        end
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          configuration.task_breakpoints = nil
+          expect(configuration.task_breakpoints).to be_nil
+        end
+      end
+    end
+
+    describe "#workflow_breakpoints" do
+      let(:custom_breakpoints) { %w[failed timeout interrupted] }
+
+      it "allows setting and getting workflow_breakpoints" do
+        configuration.workflow_breakpoints = custom_breakpoints
+        expect(configuration.workflow_breakpoints).to eq(custom_breakpoints)
+      end
+
+      context "with empty array" do
+        it "accepts empty array assignment" do
+          configuration.workflow_breakpoints = []
+          expect(configuration.workflow_breakpoints).to eq([])
+        end
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          configuration.workflow_breakpoints = nil
+          expect(configuration.workflow_breakpoints).to be_nil
+        end
+      end
+    end
+
+    describe "#logger" do
+      let(:custom_logger) { Logger.new($stderr, progname: "test") }
+
+      it "allows setting and getting logger" do
+        configuration.logger = custom_logger
+        expect(configuration.logger).to eq(custom_logger)
+      end
+
+      context "with nil value" do
+        it "accepts nil assignment" do
+          expect { configuration.logger = nil }.not_to raise_error
+          expect(configuration.logger).to be_nil
+        end
+      end
+    end
+  end
+
+  describe "#to_h" do
+    let(:result) { configuration.to_h }
+
+    it "returns hash with all configuration attributes" do
+      expect(result).to include(
+        middlewares: configuration.middlewares,
+        callbacks: configuration.callbacks,
+        coercions: configuration.coercions,
+        validators: configuration.validators,
+        task_breakpoints: configuration.task_breakpoints,
+        workflow_breakpoints: configuration.workflow_breakpoints,
+        logger: configuration.logger
+      )
+    end
+
+    context "with modified attributes" do
+      let(:custom_breakpoints) { %w[custom_failed custom_error] }
+      let(:custom_logger) { Logger.new($stderr, progname: "test") }
+
+      before do
+        configuration.task_breakpoints = custom_breakpoints
+        configuration.logger = custom_logger
+      end
+
+      it "reflects the current attribute values" do
+        expect(result[:task_breakpoints]).to eq(custom_breakpoints)
+        expect(result[:logger]).to eq(custom_logger)
+      end
+    end
+
+    context "with nil attributes" do
+      before do
+        configuration.middlewares = nil
+        configuration.logger = nil
+      end
+
+      it "includes nil values in the hash" do
+        expect(result[:middlewares]).to be_nil
+        expect(result[:logger]).to be_nil
+      end
     end
   end
 end
--- spec/cmdx_spec.rb
+++ spec/cmdx_spec.rb
@@ -1,1 +1,216 @@
 # frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CMDx do
+  after do
+    described_class.reset_configuration!
+  end
+
+  describe ".configuration" do
+    it "returns a Configuration instance" do
+      expect(described_class.configuration).to be_a(CMDx::Configuration)
+    end
+
+    it "returns the same instance on subsequent calls" do
+      first_call = described_class.configuration
+      second_call = described_class.configuration
+
+      expect(first_call).to be(second_call)
+    end
+
+    context "when @configuration is already set" do
+      let(:custom_config) { CMDx::Configuration.new }
+
+      before do
+        described_class.instance_variable_set(:@configuration, custom_config)
+      end
+
+      after do
+        # Skip the automatic reset for this test
+        described_class.instance_variable_set(:@configuration, nil)
+      end
+
+      it "returns the existing configuration" do
+        expect(described_class.configuration).to be(custom_config)
+      end
+
+      it "reuses the existing instance" do
+        first_config = described_class.configuration
+        second_config = described_class.configuration
+
+        expect(first_config).to be(second_config)
+        expect(first_config).to be(custom_config)
+      end
+    end
+
+    context "when @configuration is nil" do
+      before do
+        described_class.instance_variable_set(:@configuration, nil)
+      end
+
+      it "creates and returns a new Configuration instance" do
+        expect(described_class.configuration).to be_a(CMDx::Configuration)
+      end
+    end
+  end
+
+  describe ".configure" do
+    it "yields the configuration object" do
+      expect { |b| described_class.configure(&b) }.to yield_with_args(CMDx::Configuration)
+    end
+
+    it "returns the configuration object" do
+      result = described_class.configure { nil }
+
+      expect(result).to be(described_class.configuration)
+    end
+
+    it "allows configuration modification within the block" do
+      custom_breakpoints = %w[custom_failed timeout]
+
+      described_class.configure do |config|
+        config.task_breakpoints = custom_breakpoints
+      end
+
+      expect(described_class.configuration.task_breakpoints).to eq(custom_breakpoints)
+    end
+
+    it "passes the same configuration instance to the block" do
+      original_config = described_class.configuration
+
+      described_class.configure do |config|
+        expect(config).to be(original_config)
+      end
+    end
+
+    context "without a block" do
+      it "raises ArgumentError with descriptive message" do
+        expect { described_class.configure }.to raise_error(ArgumentError, "block required")
+      end
+    end
+
+    context "with multiple configuration changes" do
+      let(:custom_logger) { Logger.new($stderr, progname: "test") }
+      let(:custom_breakpoints) { %w[error timeout] }
+
+      it "applies all changes correctly" do
+        described_class.configure do |config|
+          config.task_breakpoints = custom_breakpoints
+          config.logger = custom_logger
+        end
+
+        config = described_class.configuration
+        expect(config.task_breakpoints).to eq(custom_breakpoints)
+        expect(config.logger).to eq(custom_logger)
+      end
+    end
+  end
+
+  describe ".reset_configuration!" do
+    let(:custom_logger) { Logger.new($stderr, progname: "test") }
+    let(:custom_breakpoints) { %w[custom_failed] }
+
+    before do
+      # Modify the configuration first
+      described_class.configure do |config|
+        config.task_breakpoints = custom_breakpoints
+        config.workflow_breakpoints = custom_breakpoints
+        config.logger = custom_logger
+      end
+    end
+
+    after do
+      # Skip the automatic reset for these tests since we're testing reset
+      described_class.instance_variable_set(:@configuration, nil)
+    end
+
+    it "creates a new Configuration instance" do
+      original_config = described_class.configuration
+      described_class.reset_configuration!
+      expect(described_class.configuration).not_to be(original_config)
+      expect(described_class.configuration).to be_a(CMDx::Configuration)
+    end
+
+    it "resets all breakpoints to default values" do
+      described_class.reset_configuration!
+      config = described_class.configuration
+
+      expect(config.task_breakpoints).to eq(%w[failed])
+      expect(config.workflow_breakpoints).to eq(%w[failed])
+    end
+
+    it "resets logger to default configuration" do
+      described_class.reset_configuration!
+      logger = described_class.configuration.logger
+
+      expect(logger.progname).to eq("cmdx")
+      expect(logger.formatter).to be_a(CMDx::LogFormatters::Line)
+      expect(logger.level).to eq(Logger::INFO)
+    end
+
+    it "resets all registries to their default state" do
+      described_class.reset_configuration!
+      config = described_class.configuration
+
+      expect(config.middlewares.registry).to be_empty
+      expect(config.callbacks.registry).to be_empty
+      expect(config.coercions.registry.keys).to include(
+        :array, :string, :integer, :boolean, :float, :hash
+      )
+      expect(config.validators.registry.keys).to include(
+        :presence, :format, :inclusion, :exclusion, :length, :numeric
+      )
+    end
+
+    it "clears the memoized @configuration variable" do
+      old_object_id = described_class.configuration.object_id
+      described_class.reset_configuration!
+
+      expect(described_class.configuration.object_id).not_to eq(old_object_id)
+    end
+
+    context "when called multiple times" do
+      it "creates a fresh instance each time" do
+        described_class.reset_configuration!
+        first_reset = described_class.configuration
+
+        described_class.reset_configuration!
+        second_reset = described_class.configuration
+
+        expect(first_reset).not_to be(second_reset)
+      end
+    end
+
+    context "when configuration is not modified" do
+      it "still creates a new instance" do
+        original_config = described_class.configuration
+        described_class.reset_configuration!
+
+        expect(described_class.configuration).not_to be(original_config)
+      end
+    end
+
+    context "when configuration has custom registries" do
+      let(:custom_middleware) { CMDx::MiddlewareRegistry.new }
+      let(:custom_callback) { CMDx::CallbackRegistry.new }
+
+      before do
+        described_class.configure do |config|
+          config.middlewares = custom_middleware
+          config.callbacks = custom_callback
+        end
+      end
+
+      it "resets to new default registry instances" do
+        described_class.reset_configuration!
+        config = described_class.configuration
+
+        expect(config.middlewares).not_to be(custom_middleware)
+        expect(config.callbacks).not_to be(custom_callback)
+        expect(config.middlewares).to be_a(CMDx::MiddlewareRegistry)
+        expect(config.callbacks).to be_a(CMDx::CallbackRegistry)
+      end
+    end
+  end
+end
