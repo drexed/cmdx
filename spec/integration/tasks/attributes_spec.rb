5a10fad73559c0b1e48a3773ec642702adc45914
Juan Gomez <drexed@users.noreply.github.com>
2025-08-14 22:59:53 -0400

    Update attributes_spec.rb

--- spec/integration/tasks/attributes_spec.rb
+++ spec/integration/tasks/attributes_spec.rb
@@ -3,6 +3,70 @@
 require "spec_helper"
 
 RSpec.describe "Task attributes", type: :feature do
+  context "when defining" do
+    context "without options" do
+      context "with no inputs" do
+        it "fails due to missing input" do
+          task = create_task_class do
+            attribute :plain_attr
+            required :required_attr
+            optional :optional_attr
+
+            def work
+              context.attrs = [plain_attr, required_attr, optional_attr]
+            end
+          end
+
+          result = task.execute
+
+          expect(result).to have_been_failure(
+            reason: "required_attr must be accessible via the source",
+            metadata: { messages: { required_attr: ["must be accessible via the source"] } },
+            cause: be_a(CMDx::FailFault)
+          )
+        end
+      end
+
+      context "with minimum inputs" do
+        it "returns attributes defined as methods" do
+          task = create_task_class do
+            attribute :plain_attr
+            required :required_attr
+            optional :optional_attr
+
+            def work
+              context.attrs = [plain_attr, required_attr, optional_attr]
+            end
+          end
+
+          result = task.execute(required_attr: "required")
+
+          expect(result).to have_been_success
+          expect(result).to have_matching_context(attrs: [nil, "required", nil])
+        end
+      end
+
+      context "with maximum inputs" do
+        it "returns attributes defined as methods" do
+          task = create_task_class do
+            attribute :plain_attr
+            required :required_attr
+            optional :optional_attr
+
+            def work
+              context.attrs = [plain_attr, required_attr, optional_attr]
+            end
+          end
+
+          result = task.execute(plain_attr: "plain", required_attr: "required", optional_attr: "optional")
+
+          expect(result).to have_been_success
+          expect(result).to have_matching_context(attrs: %w[plain required optional])
+        end
+      end
+    end
+  end
+
   context "when inheriting" do
     it "assumes the parents attributes" do
       parent_task = create_task_class(name: "ParentTask") do
