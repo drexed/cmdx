fd75c3e5da07450f3dc9c454b850cad5de8b5960
Juan Gomez <drexed@users.noreply.github.com>
2026-04-22 14:54:18 -0400

    Update runtime.rb

--- lib/cmdx/runtime.rb
+++ lib/cmdx/runtime.rb
@@ -121,7 +121,7 @@ module CMDx
         @root ? Chain.current.unshift(result) : Chain.current.push(result)
         emit_telemetry(:task_executed, result:)
         @task.logger.info do
-          exclusions = @task.settings.log_exclusions
+          exclusions = @task.class.settings.log_exclusions
           exclusions.empty? ? result : result.to_h.except(*exclusions)
         end
       end
